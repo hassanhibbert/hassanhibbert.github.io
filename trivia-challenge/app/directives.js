c196cb560c517af0ddd90aa416e7f42ac743e00a
Hassan Hibbert <hassanhibbert@gmail.com>
2019-08-31 07:55:31 -0400

    update

--- trivia-challenge/app/directives.js
+++ trivia-challenge/app/directives.js
@@ -1,79 +1,68 @@
 (function(quizApp) {
 
-    var quizDirective = function($location, $timeout) {
+  var quizDirective = function($location, $timeout) {
 
-        var link = function link(scope, element) {
-            function countdown(minutes, cb) {
-                var seconds = 60,
-                    mins = minutes;
-                var tick = function() {
-                    var current_minutes = mins - 1;
-                    seconds--;
-                    element.html(current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds));
-                    if (seconds > 0) {
-                        $timeout(tick, 1000);
-                    } else if (mins > 1) {
-                        $timeout(function() {
-                            countdown(mins - 1, cb);
-                        }, 1000);
-                    }
+    var link = function link(scope, element) {
+      function countdown(minutes, cb) {
+        var seconds = 60,
+          mins = minutes;
+        var tick = function() {
+          var current_minutes = mins - 1;
+          seconds--;
+          element.html(current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds));
+          if (seconds > 0) {
+            $timeout(tick, 1000);
+          } else if (mins > 1) {
+            $timeout(function() {
+              countdown(mins - 1, cb);
+            }, 1000);
+          }
 
-                    if (element.html() === '0:00') {
-                        cb();
-                    } else if (scope.quizSubmit === 'true') { // end timer when quiz is submitted
-                        tick = null;
-                    }
-                };
-                tick();
-            }
-
-            countdown(Number(scope.minute), function() {
-                $location.url('results');
-            });
+          if (element.html() === '0:00') {
+            cb();
+          } else if (scope.quizSubmit === 'true') { // end timer when quiz is submitted
+            tick = null;
+          }
         };
+        tick();
+      }
 
-        return {
-            scope: { minute: '@',  quizSubmit: '@' },
-            restrict: 'E',
-            link: link
-        };
+      countdown(Number(scope.minute), function() {
+        $location.url('results');
+      });
     };
 
-    var introDirective = function(animationFactory, $timeout) {
+    return {
+      scope: { minute: '@',  quizSubmit: '@' },
+      restrict: 'E',
+      link: link
+    };
+  };
 
-        function _get(selector) {
-            return document.querySelector(selector);
-        }
-        
-        var link = function link() {
-            var $circleImage = angular.element(_get('.circle')),
-                $quizTitle = angular.element(_get('.title')),
-                $startButton = angular.element(_get('.start-quiz')),
+  var introDirective = function() {
 
-                animation = function () {
-                    $circleImage.addClass('fadeInScale');
-                    $quizTitle.addClass('fadeInScale');
-                    animationFactory.transitionEnd($circleImage[0], 'transform', function () {
-                        $startButton.addClass('fadeIn');
-                    });
-                };
+    function _get(selector) {
+      return document.querySelector(selector);
+    }
 
-          $timeout(animation, 10);
-
-        };
-
-        return {
-            scope: {},
-            restrict: 'EA',
-            link: link,
-            templateUrl: 'partials/intro-template.html'
-        };
+    var link = function link() {
+      angular.element(_get('.circle')).addClass('fadeInScale');
+      angular.element(_get('.title')).addClass('fadeInScale');
+      angular.element(_get('.start-quiz')).addClass('fadeIn');
     };
 
-    // quiz timer directive
-    quizApp.directive('quizTimer', ['$location', '$timeout', quizDirective]);
+    return {
+      scope: {},
+      restrict: 'EA',
+      link: link,
+      templateUrl: 'partials/intro-template.html'
+    };
+  };
 
-    // quiz intro directive
-    quizApp.directive('quizIntro', ['animationFactory', '$timeout', introDirective]);
+  // quiz timer directive
+  quizApp.directive('quizTimer', ['$location', '$timeout', quizDirective]);
+
+  // quiz intro directive
+  quizApp.directive('quizIntro', ['animationFactory', '$timeout', introDirective]);
 
 })(angular.module('quizApp'));
